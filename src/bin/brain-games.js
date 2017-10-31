8cb37e5831df8e915d7b970aa012a0ef7bc7c569
ethelle <ethelle@yandex.ru>
2017-10-31 04:15:54 +0000

    stage 2

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
+import question from '..';
 console.log('Welcome to the Brain Games!');
+question();
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const question = () => {
+    const actual = readlineSync.question('May I have your name? ');
+    console.log("Hello, " + actual + "!");
+};
+
+export default question;
